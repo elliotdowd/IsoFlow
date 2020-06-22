2bb190687d030cf4678380bc87e304890f5c101f
elliotdowd <you@example.com>
2020-06-21 22:05:28 -0700

    commit on 'master'

--- plotting.py
+++ plotting.py
@@ -23,16 +23,18 @@ def plot_mesh(mesh):
 def plot_contour(domain, mesh, state):
     import matplotlib.pyplot as plt
     from matplotlib import cm
+    #import plotly.graph_objects as go
     import numpy as np
 
     fig, ax = plt.subplots()
     ax.set_facecolor( (0.3, 0.3, 0.3) )
 
-    cont = ax.contourf(mesh.xxc, mesh.yyc, state.Mach, cmap=cm.jet)
+    cont = ax.contourf(mesh.xxc[1:-1,1:-1], mesh.yyc[1:-1,1:-1], state.Mach[1:-1,1:-1], 500, cmap=cm.jet)
+    #fig.add_trace(go.Contour(z=z, line_smoothing=0.8), 1, 1)
     plt.gca().set_aspect('equal', adjustable='box')
 
     # colorbar settings
-    CB = fig.colorbar(cont, shrink=0.8, extend='both')
+    CB = fig.colorbar(cont, shrink=0.6, extend='both')
 
     # plot labeling 
     plt.xlabel('x-coordinate (m)')
