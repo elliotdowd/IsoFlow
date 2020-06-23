8e360e1f23af3549855717e8691b315b2ed841e8
elliotdowd <you@example.com>
2020-06-22 21:55:53 -0700

    commit on 'master'

--- fortran/split/split.f90
+++ fortran/split/split.f90
@@ -1,26 +1,38 @@
 !-*- f90 -*- -
-function Mm( M )
+function Mm( M, p, q )
 
     implicit none
     integer, parameter :: dp=kind(0.d0)
 
-    real(kind=8), intent(in) :: M(:,:)
-    real(kind=8) :: Mm(:,:)
+    integer :: i, j
+    integer, intent(in) :: p, q
+    real(kind=8), intent(in) :: M(p,q)
+    real(kind=8) :: Mm(p,q)
 
-    Mm = 0.5_dp * ( M-abs(M) )
+    do i = 1, p
+        do j = 1, q
+            Mm(i,j) = 0.5_dp * ( M(i,j)-abs(M(i,j)) )
+        end do
+    end do
 
 end function
 
 
 !-*- f90 -*- -
-function Mp( M )
+function Mp( M, p, q )
 
     implicit none
     integer, parameter :: dp=kind(0.d0)
 
-    real(kind=8), intent(in) :: M(:,:)
-    real(kind=8) :: Mm(:,:)
+    integer :: i, j
+    integer, intent(in) :: p, q
+    real(kind=8), intent(in) :: M(p,q)
+    real(kind=8) :: Mp(p,q)
 
-    Mm = 0.5_dp * ( M+abs(M) )
+    do i = 1, p
+        do j = 1, q
+            Mp(i,j) = 0.5_dp * ( M(i,j)+abs(M(i,j)) )
+        end do
+    end do
 
 end function
